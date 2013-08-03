790dc250e0c21d1624d269f3b7360caf73e70662
chris <ifririt@gmail.com>
2013-08-03 16:57:38 -0400

    Menu actualizado

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -19,11 +19,16 @@
         array(
             'class'=>'bootstrap.widgets.TbMenu',
             'items'=>array(
-                array('label'=>'Home', 'url'=>array('/site/index')),
+            	array('label'=>'Home', 'url'=>array('/site/index')),
+                array('label'=>'Productos', 'url'=>array('/producto/index')),
+                array('label'=>'Categorías', 'url'=>array('/categoria/index')),
+                /*
                 array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
                 array('label'=>'Contact', 'url'=>array('/site/contact')),
+                */
                 array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
                 array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+                
             ),
         ),
     ),
